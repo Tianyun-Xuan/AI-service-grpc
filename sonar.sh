d6b51a25e96c92501df1d227b3136f22ed6027c5
TianyunXuan <x523356708@gmail.com>
2023-09-04 15:09:50 +0800

    adapte sonar 9.9.1

--- sonar.sh
+++ sonar.sh
@@ -6,14 +6,23 @@ flake8 --config=config/.flake8 ai_grpc.py >.reports/flake8-report.txt
 # unittest
 pytest -v -s --cov=. --cov-report xml:.reports/coverage.xml --junitxml=.reports/nosetests.xml
 
-# sonar
+# sonar 9.9.1
 /home/xavier/Codes/sonar-scanner-5.0.1.3006-linux/bin/sonar-scanner -X \
   -Dsonar.verbose=true \
   -Dsonar.projectKey=AI-service-grpc \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.token=sqp_d6efb8d7618b22fb06d28ae2fda12f691219cdc6 \
+  -Dsonar.login=sqp_5a9a2565094c1d25fc1a066548656f488dcb056c \
   &>.logs/sonar-scanner.log
+
+# sonar 10.0.1
+# /home/xavier/Codes/sonar-scanner-5.0.1.3006-linux/bin/sonar-scanner -X \
+#   -Dsonar.verbose=true \
+#   -Dsonar.projectKey=AI-service-grpc \
+#   -Dsonar.sources=. \
+#   -Dsonar.host.url=http://localhost:9000 \
+#   -Dsonar.token=sqp_5a9a2565094c1d25fc1a066548656f488dcb056c \
+#   &>.logs/sonar-scanner.log
 
 # clean up
 rm -rf .coverage
